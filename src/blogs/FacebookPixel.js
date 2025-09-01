bfb0ddf7d29778c3acd9aa30e3770ded063aeaa9
addityaasharma <adityasharma96655@gmail.com>
2025-09-01 15:22:00 +0530

    changs doen

--- src/blogs/FacebookPixel.js
+++ src/blogs/FacebookPixel.js
@@ -1,5 +1,18 @@
+// facebookPixel.js
+
+// Track PageView
 export const pageView = () => {
-    if (window.fbq) {
-        window.fbq("track", "PageView");
-    }
+  if (window.fbq) {
+    window.fbq("track", "PageView");
+  }
+};
+
+// Track Purchase
+export const purchase = (value, currency = "USD") => {
+  if (window.fbq) {
+    window.fbq("track", "Purchase", {
+      value: value,
+      currency: currency,
+    });
+  }
 };
